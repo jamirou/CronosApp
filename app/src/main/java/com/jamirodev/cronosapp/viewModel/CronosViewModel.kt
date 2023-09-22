ce368dba068903a1f456885cf18ff9515923878c
Jamiro <jamirocristian15@gmail.com>
2023-09-22 18:21:47 -0300

    finish db from viewmodel.

---
+++ app/src/main/java/com/jamirodev/cronosapp/viewModel/CronosViewModel.kt
@@ -0,0 +1,35 @@
+package com.jamirodev.cronosapp.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jamirodev.cronosapp.model.Cronos
+import com.jamirodev.cronosapp.repository.CronosRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class CronosViewModel @Inject constructor(private val repository: CronosRepository): ViewModel() {
+
+    private val _cronosList = MutableStateFlow<List<Cronos>>(emptyList())
+    val cronosList = _cronosList.asStateFlow()
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAllCronos().collect{item ->
+                if (item.isNullOrEmpty()) {
+                    _cronosList.value = emptyList()
+                }else {
+                    _cronosList.value = item
+                }
+            }
+        }
+    }
+    fun addCrono(crono: Cronos) = viewModelScope.launch { repository.addCrono(crono) }
+    fun updateCrono(crono: Cronos) = viewModelScope.launch { repository.updateCrono(crono) }
+    fun deleteCrono(crono: Cronos) = viewModelScope.launch { repository.deleteCrono(crono) }
+}
