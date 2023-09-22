6ff1594cb3a53f7e35dbeb918a4e3b4da47fbe6d
Jamiro <jamirocristian15@gmail.com>
2023-09-22 13:45:08 -0300

    added dependences and using ksp.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id ("com.google.dagger.hilt.android")
+
 }
 
 android {
     namespace = "com.jamirodev.cronosapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jamirodev.cronosapp"
@@ -30,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -51,7 +54,21 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Room
+    implementation ("androidx.room:room-ktx:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+
+    //  Dagger Hilt
+    implementation ("com.google.dagger:hilt-android:2.46.1")
+    ksp("com.google.dagger:hilt-compiler:2.46.1")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.3")
+
+    // Swipe
+    implementation ("me.saket.swipe:swipe:1.1.1")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
