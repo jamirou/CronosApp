27cfe471af8616c103b848b1e888aee08be3a632
Jamiro <jamirocristian15@gmail.com>
2023-09-24 12:58:35 -0300

    added chronos logic in viewmodel.

--- app/src/main/java/com/jamirodev/cronosapp/viewModel/ChronometerViewModel.kt
+++ app/src/main/java/com/jamirodev/cronosapp/viewModel/ChronometerViewModel.kt
@@ -5,8 +5,11 @@ import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jamirodev.cronosapp.state.CronoState
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ChronometerViewModel: ViewModel() {
     var state by mutableStateOf(CronoState())
@@ -19,4 +22,47 @@ class ChronometerViewModel: ViewModel() {
     fun onValue(value: String) {
         state = state.copy(title = value)
     }
+
+    fun init() {
+        state = state.copy(
+            chronometerActive = true
+        )
+    }
+    fun pause() {
+        state = state.copy(
+            chronometerActive = false,
+            showSaveButton = true
+        )
+    }
+    fun stop() {
+        cronJob?.cancel()
+        time = 0
+        state = state.copy(
+            chronometerActive = false,
+            showSaveButton = false,
+            showTextField = false
+        )
+    }
+    fun showTextField() {
+        state = state.copy(
+            showTextField = true
+        )
+    }
+    fun chronos() {
+        if (state.chronometerActive) {
+            cronJob?.cancel()
+            cronJob = viewModelScope.launch {
+                while (true) {
+                    delay(1000)
+                    time += 1000
+                }
+            }
+        }else{
+            cronJob?.cancel()
+        }
+    }
+
 }
+
+
+
